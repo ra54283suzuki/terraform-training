72defb769a583b9f91136f563446d5f61543059b
ra54283suzuki <ra54283suzuki@gmail.com>
2022-11-28 22:01:35 +0900

    ストレージを追加

--- main.tf
+++ main.tf
@@ -17,3 +17,76 @@ data "aws_iam_policy_document" "allow_describe_regions" {
     resources = ["*"]
   }
 }
+
+#### プライベートバケットの定義
+resource "aws_s3_bucket" "private" {
+  bucket = "private-pragmatic-terraform"
+  
+  # バージョニング設定
+  versioning {
+    enabled = true
+  }
+  
+  # 暗号化設定
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+#### ブロックパブリックアクセスの定義
+resource "aws_s3_bucket_public_access_block" "private" {
+  bucket = aws_s3_bucket.private.id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
+#### パブリックバケットの定義
+resource "aws_s3_bucket" "public" {
+  bucket = "public-pragmatic-terraform"
+  acl = "public-read"
+
+  cors_rule {
+    allowed_origins = ["https://example.com"]
+    allowed_methods = ["GET"]
+    allowed_headers = ["*"]
+    max_age_seconds = 3000
+  }
+}
+
+#### ログバケットの定義
+resource "aws_s3_bucket" "alb_log" {
+  bucket = "alb-log-pragmatic-terraform"
+  
+  lifecycle_rule {
+    enabled = true
+    
+    expiration {
+      days = "180"
+    }
+  }
+}
+
+#### バケットポリシーの定義
+resource "aws_s3_bucket_policy" "alb_log" {
+  bucket = aws_s3_bucket.alb_log.id
+  policy = data.aws_iam_policy_document.alb_log.json
+}
+
+data "aws_iam_policy_document" "alb_log" {
+  statement {
+    effect = "Allow"
+    actions = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*"]
+
+    principals {
+      type = "AWS"
+      identifiers = ["582318560864"]
+    }
+  }
+}
