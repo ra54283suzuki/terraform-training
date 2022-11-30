4fd2355bb2abf36de16668d2b4fbb95531b8dde5
ra54283suzuki <ra54283suzuki@gmail.com>
2022-11-30 23:45:20 +0900

    ロードバランサーの追加

--- main.tf
+++ main.tf
@@ -17,6 +17,30 @@ module "example_sg" {
   cidr_blocks = ["0.0.0.0/0"]
 }
 
+module "http_sg" {
+  source = "./security_group"
+  name = "http-sg"
+  vpc_id = "aws_vpc.example.id"
+  port = 80
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "https_sg" {
+  source = "./security_group"
+  name = "https-sg"
+  vpc_id = "aws_vpc.example.id"
+  port = 443
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "http_redirect_sg" {
+  source = "./security_group"
+  name = "https-redirect-sg"
+  vpc_id = "aws_vpc.example.id"
+  port = 8080
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
 #### ポリシードキュメントの定義
 data "aws_iam_policy_document" "allow_describe_regions" {
   statement {
@@ -299,4 +323,49 @@ resource "aws_security_group_rule" "egress_example" {
   protocol = "-1"
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.example.id
+}
+
+#### アプリケーションロードバランサーの定義
+resource "aws_lb" "example" {
+  name = "example"
+  load_balancer_type = "application"
+  internal = false
+  idle_timeout = 60
+  enable_deletion_protection = true
+
+  subnets = [
+    aws_subnet.public_0.id,
+    aws_subnet.public_1.id,
+  ]
+
+  access_logs {
+    bucket = aws_s3_bucket.alb_log.id
+  }
+
+  security_groups = [
+    module.http_sg.security_group_id,
+    module.https_sg.security_group_id,
+    module.http_redirect_sg.security_group_id,
+  ]
+}
+
+output "alb_dns_name" {
+  value = aws_lb.example.dns_name
+}
+
+#### HTTPリスナーの定義
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port = "80"
+  protocol = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "これは『HTTP』です"
+      status_code = "200"
+    }
+  }
 }
