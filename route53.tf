1e8b31d0747c6d5615d77973298414f863993633
ra54283suzuki <ra54283suzuki@gmail.com>
2022-12-03 23:56:27 +0900

    Route53を追加

---
+++ route53.tf
@@ -0,0 +1,32 @@
+#### ドメイン登録完了するまでコメントアウトS
+
+/*
+
+#### ホストゾーンのデータソース定義
+data "aws_route53_zone" "example" {
+  name = "example.com"
+}
+
+#### ホストゾーンのリソース定義
+resource "aws_route53_zone" "test_example" {
+  name = "test.example.com"
+}
+
+#### ALBのDNSレコードの定義
+resource "aws_route53_record" "example" {
+  zone_id = data.aws_route53_zone.example.zone_id
+  name = data.aws_route53_zone.example.name
+  type = "A"
+
+  alias {
+    name = aws_lb.example.dns_name
+    zone_id = aws_lb.example.zone_id
+    evaluate_target_health = true
+  }
+}
+
+output "domain_name" {
+  value = aws_route53_record.example.name
+}
+
+*/
