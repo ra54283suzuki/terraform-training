4cd03a393311ae6f7cf80545fd783203b45e90c2
ra54283suzuki <ra54283suzuki@gmail.com>
2022-12-03 11:20:16 +0900

    ファイル分割

---
+++ iam.tf
@@ -0,0 +1,8 @@
+#### ポリシードキュメントの定義
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect = "Allow"
+    actions = ["ec2:DescribeRegions"] # リージョン一覧を取得する
+    resources = ["*"]
+  }
+}
